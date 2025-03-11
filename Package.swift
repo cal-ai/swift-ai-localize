f418960f88a81adb024a51c61e68839bb1c5c2d3
Henry Langmack <henry.langmack@gmail.com>
2025-03-11 00:22:27 -0400

    update package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "swift-ai-localize",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v11)
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
